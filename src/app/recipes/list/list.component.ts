5c5fedb8ebb4b1c1045d6c51fa1085e73bf0ee04
John Desrosiers <john.desro@gmail.com>
2018-10-11 10:16:39 -0700

    Render recipe data for each recipe in list
    Modify RecipeListComponent to render each recipe in an array of recipes.
    This change adds a recipes property to the component class. A Recipe
    model is created for representing recipe data. There are 3 public
    properties on the Recipe class:
      - name: string - title for the recipe
      - description: string
      - imagePath: string - url of image associated to the recipe
    There is a constructor which accepts three arguments: name, description,
    and imagePath. The arguments are used to set the initial value for the
    public properties.

--- src/app/recipes/list/list.component.ts
+++ src/app/recipes/list/list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test.',
+      'http://assets.kraftfoods.com/recipe_images/opendeploy/54852_640x428.jpg'),
+  ];
 
   constructor() { }
 
