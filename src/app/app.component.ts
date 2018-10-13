8566728f75812df960d26ab63fa8a5cc87fb5edb
John Desrosiers <john.desro@gmail.com>
2018-10-13 07:45:33 -0700

    Click on navs to navigate between pages
    Add events and handling to AppComponent and HeaderComponent to show
    the Recipes and Shopping List pages when user clicks on nav links in
    the page header.
    
    - Header emits goTo<string> when a nav link is clicked.
    - App shows either Recipes or Shopping List page when goTo event is
      emitted by the HeaderComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage = 'recipes';
+
+  goToPage(pageName: string) {
+    this.currentPage = pageName;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  @Output() goToPage = new EventEmitter<string>();
+
+  navClick(pageName: string) {
+    this.goToPage.emit(pageName);
+  }
+}
