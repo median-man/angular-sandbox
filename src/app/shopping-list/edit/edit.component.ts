856c35f014de93fb3dcc1c497cbce1722c3c0328
John Desrosiers <john.desro@gmail.com>
2018-10-15 14:37:41 -0700

    Define ShoppingListService and implement
    Create ShoppingListService. Implement the service to manage
    Ingredients in the shopping list.
    
    ShoppingListService#getIngredients returns an array of Ingredients
    which make up the shopping list.
    
    ShoppingListService#ingredientChange is an EventEmitter which emits
    an array for the current list of Ingredients.
    
    ShoppingListService#addIngredient(ingredient: Ingredient) adds
    an Ingredient to the shopping list and emits ingredientChange.
    
    Shopping-list-component initializes with the shopping list
    Ingredients from the service and subscribes to the ingredientChange
    event to update the shopping list items that are rendered.
    
    Shopping-list-edit component calls addIngredient when user adds
    a new ingredient.

--- src/app/shopping-list/edit/edit.component.ts
+++ src/app/shopping-list/edit/edit.component.ts
@@ -1,21 +1,23 @@
-import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './edit.component.html',
 })
 
 export class ShoppingListEditComponent {
-  @Output() addIngredient = new EventEmitter<Ingredient>();
-
   @ViewChild('nameInput') nameInput: ElementRef;
   @ViewChild('amountInput') amountInput: ElementRef;
+
+  constructor(private shoppingListService: ShoppingListService) { }
 
   addBtnClick() {
     const name = this.nameInput.nativeElement.value;
     const amount = this.amountInput.nativeElement.value;
     const ingredient = new Ingredient(name, amount);
-    this.addIngredient.emit(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
-export class ShoppingListService { }
+
+export class ShoppingListService {
+  ingredientChange = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 2),
+  ];
+
+  getIngredients = () => [...this.ingredients];
+
+  addIngredient = (ingredient: Ingredient) => {
+    this.ingredients.push(ingredient);
+    this.ingredientChange.emit(this.getIngredients());
+  }
+}
