e5e90697b6cb78b5e54a0618e89716c6db7e0391
John Desrosiers <john.desro@gmail.com>
2018-10-15 18:29:46 -0700

    User can add recipe ingredients to shopping list
    All ingredients for a recipe are added to the shopping list when the
    Manage Recipe>To Shopping List control is clicked. This is done
    by calling addIngredientsToShopping list method on the detail component,
    which adds the ingredients using the ShoppingListService.
    
    Add ingredients: Ingredient[] to recipe.model.
    
    Recipe Detail component depends on ShoppingService instance provided
    by Angular.

--- src/app/recipes/detail/detail.component.ts
+++ src/app/recipes/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,4 +10,10 @@ import { Recipe } from '../recipe.model';
 
 export class DetailComponent {
   @Input() recipe: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  addIngredientsToShoppingList = () => {
+    this.shoppingListService.addIngredient(...this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
@@ -8,13 +9,24 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is just a test.',
-      'http://assets.kraftfoods.com/recipe_images/opendeploy/54852_640x428.jpg'),
+      'Halloween Bar',
+      'This is a tasty halloween cookie bar.',
+      'http://assets.kraftfoods.com/recipe_images/opendeploy/54852_640x428.jpg',
+      [
+        new Ingredient('sugar', 2),
+        new Ingredient('bag of candy corn', 1)
+      ]
+    ),
     new Recipe(
-      'Another Test',
-      'This is just a test.',
-      'http://assets.kraftfoods.com/recipe_images/opendeploy/54852_640x428.jpg'),
+      'Chocolate Chip Cookies',
+      'Classic favorite.',
+      'https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg',
+      [
+        new Ingredient('bag of chocolate chips', 1),
+        new Ingredient('flour', 4),
+        new Ingredient('butter', 1)
+      ]
+    ),
   ];
 
   getRecipes = () => [...this.recipes];
