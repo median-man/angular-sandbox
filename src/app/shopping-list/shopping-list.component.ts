b7ab80a8d19df86dd4b7e5894d996d1859031ee2
John Desrosiers <john.desro@gmail.com>
2018-10-18 07:54:35 -0700

    Refactor shopping list service ingredientChange
    Implement rxjs Subject for ingredientChange event of the shopping list
    service. This change replaces the EventEmitter that was used previously.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -8,16 +10,22 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css']
 })
 
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   private ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientChange.subscribe((ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    });
+    this.subscription = this.shoppingListService
+      .ingredientChange
+      .subscribe((ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,21 +1,24 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({ providedIn: 'root' })
 
 export class ShoppingListService {
-  ingredientChange = new EventEmitter<Ingredient[]>();
+  ingredientChange = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 2),
   ];
 
-  getIngredients = () => [...this.ingredients];
+  private copyIngredient = (ingredient: Ingredient) => Object.assign({}, ingredient);
+
+  getIngredients = () => this.ingredients.map(this.copyIngredient);
 
   addIngredient = (...ingredients: Ingredient[]) => {
-    this.ingredients.push(...ingredients);
-    this.ingredientChange.emit(this.getIngredients());
+    this.ingredients.push(...ingredients.map(this.copyIngredient));
+    this.ingredientChange.next(this.getIngredients());
   }
 }
