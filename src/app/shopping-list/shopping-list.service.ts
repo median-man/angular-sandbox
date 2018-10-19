92ae9b8f7a937c3b32c4ad6df8e365ef5b260d3f
John Desrosiers <john.desro@gmail.com>
2018-10-19 12:09:52 -0700

    Add features to shopping list service
    Add editItem: Subject property to shopping list service. This
    will emit a number which corresponds to the index in #ingredients
    for the ingredit opened for editing.
    
    Add getIngredientAt, changeIngredientAt, and removeIngredientAt
    methods to shopping-list service.
    
    getIngredientAt(index: number): Ingredient returns the ingredient
    at the given index in #ingredients.
    
    removeIngredientAt(index: number): Ingredient removes the element
    for the given index in #ingredients and emits ingredientChange
    with #ingredients after the change has taken place.
    
    changeIngredientAt(ingredient: Ingredient, index: number): self
    replaces the element for the given index in #ingredients. This
    method may be chained. (Returns instance of itself.)

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,6 +7,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientChange = new Subject<Ingredient[]>();
+  editItem = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -17,8 +18,23 @@ export class ShoppingListService {
 
   getIngredients = () => this.ingredients.map(this.copyIngredient);
 
+  getIngredientAt = (index: number) => this.copyIngredient(this.ingredients[index]);
+
   addIngredient = (...ingredients: Ingredient[]) => {
     this.ingredients.push(...ingredients.map(this.copyIngredient));
     this.ingredientChange.next(this.getIngredients());
+    return this;
+  }
+
+  changeIngredientAt = (ingredient: Ingredient, index: number) => {
+    this.ingredients[index] = ingredient;
+    this.ingredientChange.next(this.getIngredients());
+    return this;
+  }
+
+  removeIngredientAt = (index: number) => {
+    this.ingredients = this.ingredients.filter((el, i) => i !== index);
+    this.ingredientChange.next(this.getIngredients());
+    return this;
   }
 }
