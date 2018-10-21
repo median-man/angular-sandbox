9d8f55d3cd381c3f6a979d294a52273a0c4c8aa9
John Desrosiers <john.desro@gmail.com>
2018-10-21 10:37:25 -0700

    Delete recipe from manage recipe drop down ctrl
    The "Delete recipe" item in the manage recipe control of the recipe
    detail view will remove the recipe from the recipe list and navigate
    to the parent url path. (/recipes)
    
    Remove recipe service from RecipesComponent providers. This
    prevents the loss changes to recipes when navigating away from the
    recipe book feature.

--- src/app/recipes/detail/detail.component.ts
+++ src/app/recipes/detail/detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 import { Recipe } from '../recipe.model';
@@ -16,7 +16,8 @@ export class DetailComponent implements OnInit {
   constructor(
     private shoppingListService: ShoppingListService,
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -27,5 +28,10 @@ export class DetailComponent implements OnInit {
 
   addIngredientsToShoppingList = () => {
     this.shoppingListService.addIngredient(...this.recipe.ingredients);
+  }
+
+  onDelete() {
+    this.recipeService.deleteRecipe(this.recipe.id);
+    this.router.navigate(['..'], { relativeTo: this.route });
   }
 }
