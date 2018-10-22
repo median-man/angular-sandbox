84185a90d8103fa8b48c1be889c93c3c88364a8d
John Desrosiers <john.desro@gmail.com>
2018-10-22 10:52:32 -0700

    Initialize firebase app when app starts
    Use the firebase sdk to initialize the firebase app when
    AppComponent.ngOnInit() is called.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAUo2z0lamzjTOHYMyxTw2dAFTox8qzWYc',
+      authDomain: 'angular-sandbox-5fd45.firebaseapp.com',
+    });
+  }
+}
