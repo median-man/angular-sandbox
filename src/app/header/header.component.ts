4e6a6b3b118c61ec012f6c007571ec656cda8043
John Desrosiers <john.desro@gmail.com>
2018-10-22 06:15:32 -0700

    Handle save data and fetch data controls
    Bind methods to the click event on the "Fetch Data" and "Save Data"
    controls of the "Manage" dropdown in the header.
    
    Clicking "Fetch Data" updates the recipe data from the database.
    
    Clicking "Save Data" updates the recipe data stored in firebase from the
    current recipes in the app.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,23 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { stringify } from 'querystring';
+import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  constructor(private dataStorageSvc: DataStorageService) { }
+
+  onSaveData = () => this.dataStorageSvc
+    .storeRecipes()
+    .subscribe(
+      (data) => console.log(data)
+    )
+
+  onFetchData = () => this.dataStorageSvc
+    .getRecipes()
+    .subscribe(
+      (data) => console.log(data)
+    )
+}
