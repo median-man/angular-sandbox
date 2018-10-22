0635918d6dd199e132186c6993dafe7aa6544f73
John Desrosiers <john.desro@gmail.com>
2018-10-22 11:02:41 -0700

    Create AuthService module
    Create AuthService to handle user authentication with the
    firebase sdk.
    
    Create signupUser method which accepts an email: string, and
    password: string and creates a new user account with the firebase
    sdk. The error is logged in the console if the create user promise
    from firebase rejects. Call authService.signupUser(email, password)
    to create a new user account.
    
    Service depends on firebase/app and firebase/auth.

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  signupUser(email: string, password: string) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(console.log);
+  }
+}
