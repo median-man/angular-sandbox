40bed3b0f47a72a728a471baf5ecaff0061c33d6
John Desrosiers <john.desro@gmail.com>
2018-10-24 08:22:49 -0700

    Refactor app to use CoreModule
    Refactor app/ modules to use CoreModule instead of declaring
    core components rendered in AppComponent in AppModule.
    
    Remove redundant components from app/ that are defined in CoreModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     SharedModule,
     AuthModule,
@@ -24,6 +19,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
