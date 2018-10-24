a862c32cda1609dcd3999a19d5c2113e6dfe73bc
John Desrosiers <john.desro@gmail.com>
2018-10-24 06:39:24 -0700

    Refactor AppModule with AuthModule and routing
    Refactor AppModule and AppRoutingModule so that AuthModule and
    AuthRoutingModule replaces components, modules, and routes defined
    in the auth feature modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     SharedModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
     HttpClientModule,
   ],
