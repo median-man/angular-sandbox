35074173ff3ea31b0951cce9795d66b5c94df24b
John Desrosiers <john.desro@gmail.com>
2018-10-24 06:10:23 -0700

    Refactor AppModule to with ShoppingListModule
    Factor out modules and components in AppModule which are handled by
    ShoppingListModule and import the module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     SharedModule,
     RecipesModule,
+    ShoppingListModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
