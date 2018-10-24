321e84092096dea618fa7612a9c83b2538eb24fc
John Desrosiers <john.desro@gmail.com>
2018-10-24 05:56:23 -0700

    Refactor AppModule to implement RecipesModule
    Refactor AppModule to import the RecipesModule. Remove all imports and
    declarations that are handled by the RecipesModule.
    
    Likewise, refactor AppRoutingModule by implementing RecipesRoutingModule
    to handle all routes concerning the recipes feature.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { RecipesModule } from './recipes/recipes.module';
+
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/edit/edit.component';
-import { RecipeBookComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/list/list.component';
-import { ItemComponent } from './recipes/list/item/item.component';
-import { DetailComponent } from './recipes/detail/detail.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { AppRoutingModule } from './app-routing.module';
-import { DetailPlaceholderComponent } from './recipes/detail-placeholder/detail-placeholder.component';
-import { EditRecipeComponent } from './recipes/edit-recipe/edit-recipe.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -24,20 +20,14 @@ import { SigninComponent } from './auth/signin/signin.component';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeBookComponent,
-    RecipeListComponent,
-    ItemComponent,
-    DetailComponent,
-    DropdownDirective,
-    DetailPlaceholderComponent,
-    EditRecipeComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
+    SharedModule,
+    RecipesModule,
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
