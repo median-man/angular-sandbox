466b7b7d29ad428947da2939d6952956bb1dee8e
John Desrosiers <john.desro@gmail.com>
2018-10-26 15:44:33 -0700

    refactor: Header uses recipe store
    The onFetch and onSave methods dispatch actions to the recipe store
    instead of using DataStorageService.
    
    This commit removes data-storage.service from header.component dependencies.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AppState } from 'src/app/store/app.reducers';
 import { Logout } from 'src/app/auth/store/auth.actions';
-import { DataStorageService } from 'src/app/shared/data-storage.service';
-import { map } from 'rxjs/operators';
+import { FetchRecipes, SaveRecipes } from 'src/app/recipes/store/recipe.actions';
 
 
 @Component({
@@ -16,26 +16,15 @@ import { map } from 'rxjs/operators';
 export class HeaderComponent {
   isAuthenticated: Observable<boolean>;
 
-  constructor(
-    private store: Store<AppState>,
-    private dataStorageSvc: DataStorageService,
-  ) {
+  constructor(private store: Store<AppState>) {
     this.isAuthenticated = this.store
       .select('auth')
       .pipe(map(({ isAuthenticated }) => isAuthenticated));
   }
 
-  onSaveData = () => this.dataStorageSvc
-    .storeRecipes()
-    .subscribe(
-      (data) => console.log(data)
-    )
+  onSaveData = () => this.store.dispatch(new SaveRecipes());
 
-  onFetchData = () => this.dataStorageSvc
-    .getRecipes()
-    .subscribe(
-      (data) => console.log(data)
-    )
+  onFetchData = () => this.store.dispatch(new FetchRecipes());
 
   onSignout = () => this.store.dispatch(new Logout());
 }
