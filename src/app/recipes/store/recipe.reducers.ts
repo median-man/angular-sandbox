81b45795c625803cf91cccfbaa452a216cdc9b5a
John Desrosiers <john.desro@gmail.com>
2018-10-26 11:19:36 -0700

    feat: recipeReducers handles new actions
    This commit adds handling of the following action types to recipe reducer:
      - SET_RECIPES
      - ADD_RECIPE
      - UPDATE_RECIPE
      - DELETE_RECIPE

--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,6 +1,7 @@
 import { AppState } from 'src/app/store/app.reducers';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe.model';
+import { RecipeActions, SET_RECIPES, ADD_RECIPE, UPDATE_RECIPE, DELETE_RECIPE } from './recipe.actions';
 
 export interface FeatureState extends AppState {
   recipes: State;
@@ -36,6 +37,67 @@ const initialState = {
   ]
 };
 
-export function recipeReducer(state = initialState, action) {
-  return state;
+export function recipeReducer(state = initialState, action: RecipeActions) {
+  switch (action.type) {
+    case SET_RECIPES:
+      return setRecipes(state, action);
+
+    case ADD_RECIPE:
+      return addRecipe(state, action);
+
+    case UPDATE_RECIPE:
+      return updateRecipe(state, action);
+
+    case DELETE_RECIPE:
+      return deleteRecipe(state, action);
+
+    default:
+      return state;
+  }
+}
+
+function setRecipes(state, action) {
+  const recipes = [...action.payload];
+  return {
+    ...state,
+    recipes,
+  };
+}
+
+function addRecipe(state, action) {
+  const id = newRecipeId(state.recipes);
+  const recipes = [...state.recipes, { ...action.payload, id }];
+  return {
+    ...state,
+    recipes,
+  };
+}
+
+function newRecipeId(recipes) {
+  const initialId = '1';
+  const maxId = recipes.reduce((max, { id }) => id > max ? id : max, initialId);
+  return (parseFloat(maxId) + 1).toString();
+}
+
+function updateRecipe(state, action) {
+  const { payload: updatedRecipe } = action;
+  const recipes = state.recipes.map((recipe) => {
+    if (recipe.id === updatedRecipe.id) {
+      return { ...recipe, ...updatedRecipe };
+    }
+    return recipe;
+  });
+  return {
+    ...state,
+    recipes,
+  };
+}
+
+function deleteRecipe(state, action) {
+  const { payload: targetId } = action;
+  const recipes = state.recipes.filter(({ id }) => targetId !== id);
+  return {
+    ...state,
+    recipes,
+  };
 }
